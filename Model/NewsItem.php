de6845c11aebbf7484b9f170505b5fca87430f01
Ruben Barilani <ruben.barilani.dev@gmail.com>
2015-03-11 14:00:28 +0100

    adds publish date field

--- Model/NewsItem.php
+++ Model/NewsItem.php
@@ -9,11 +9,35 @@ use Truelab\KottiModelBundle\TypeInfo\TypeInfo;
  * @TypeInfo({
  *   "table" = "news_items",
  *   "type"  = "news_item",
+ *   "fields" = {"publish_date"},
  *   "associated_table" = "documents",
  *   "association" = "news_items.id = documents.id"
  * })
  */
 class NewsItem extends Document
 {
+    /**
+     * @var \DateTime
+     */
+    protected $publishDate;
 
+    /**
+     * @param string|\Datetime $publishDate
+     */
+    public function setPublishDate($publishDate)
+    {
+        if($publishDate instanceof \DateTime) {
+            $this->publishDate = $publishDate;
+        }else{
+            $this->publishDate = new \DateTime($publishDate);
+        }
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getPublishDate()
+    {
+        return $this->publishDate;
+    }
 }
